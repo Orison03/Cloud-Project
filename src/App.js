3ee392966a3d3122ed4bada47e2774a47a3a4b5b
Orison <orisontetteh@gmail.com>
2023-01-20 00:43:40 +0000

    worked on about

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
+import About from "./components/About";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <Services/>
+      <About/>
     </div>
   );
 }
