0c67ecd9c05ef778a6edfb25f49815753ecca2d6
Orison <orisontetteh@gmail.com>
2023-01-20 00:52:01 +0000

    worked on about cards

--- src/components/About.js
+++ src/components/About.js
@@ -5,13 +5,13 @@ function About() {
   return (
     // cover or background
     <Fade bottom big>
-      <div className="w-[80%] mx-auto drop-shadow-md border-2 border-none-500 flex flex-col py-10 rounded-3xl my-[8%]">
+      <div className="w-[80%] mx-auto border-none-500 flex flex-col py-10 my-[8%]">
         {/* description */}
         <div className="text-center">
           <h2 className="pb-4 text-lg font-semibold tracking-wide">
             Trusted by developers across the world
           </h2>
-          <p className="w-[70%] mx-auto text-base">
+          <p className="w-[95%] mx-auto text-base">
             Without a competent IT staff in place, it's difficult for any cloud
             management strategy to succeed. These individuals must possess
             knowledge of the proper tools and best practices while they keep in
@@ -21,19 +21,19 @@ function About() {
         {/* about grid block */}
         <div className="grid text-center mt-6 md:grid-cols-3">
           {/* start of block */}
-          <div className="w-[50%] mx-auto bg-purple-500 py-8 mb-4 md:w-[70%] capitalize rounded-lg">
+          <div className="w-[80%] mx-auto bg-purple-500 py-8 mb-4 md:w-[80%] capitalize rounded-lg">
             <p className="text-4xl text-white">100%</p>
             <p className="tracking-wider text-white">completion</p>
           </div>
           {/* end of block */}
           {/* start of block */}
-          <div className="w-[50%] mx-auto bg-purple-500 py-8 mb-4 capitalize md:w-[70%] rounded-lg">
+          <div className="w-[80%] mx-auto bg-purple-500 py-8 mb-4 capitalize md:w-[80%] rounded-lg">
             <p className="text-4xl text-white">24/7</p>
             <p className="tracking-wider text-white">delivery</p>
           </div>
           {/* end of block */}
           {/* start of block */}
-          <div className="w-[50%] mx-auto bg-purple-500 py-8 mb-4 md:w-[70%] text-white capitalize rounded-lg">
+          <div className="w-[80%] mx-auto bg-purple-500 py-8 mb-4 md:w-[80%] text-white capitalize rounded-lg">
             <p className="text-4xl">100k</p>
             <p className="tracking-wider">transactions</p>
           </div>
